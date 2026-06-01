21358b160de115aee270863cdaf7544e384b3aa4
Robert Snyder <robertsnyder@Roberts-MacBook-Pro.local>
2026-05-31 20:56:15 -0400

    Linter.

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from alembic import context
 
 from sqlmodel import SQLModel
 
-from model.dao.student import Student
+from model.dao.student import Student  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -68,9 +68,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- core/config.py
+++ core/config.py
@@ -1,3 +1,3 @@
 import os
 
-connection_string = os.getenv(DATABASE_URL, "postgresql://admin:admin@db:5432/mydb")
+connection_string = os.getenv("DATABASE_URL", "postgresql://admin:admin@db:5432/mydb")

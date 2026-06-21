ed30423ebd996204e75d0e2d1532ed757ecb7f82
Robert Snyder <robertsnyder@Roberts-MacBook-Pro.local>
2026-06-21 16:16:16 -0400

    Add CD workflow.

--- alembic/env.py
+++ alembic/env.py
@@ -7,11 +7,18 @@ from alembic import context
 
 from sqlmodel import SQLModel
 
+import os
+
 from model.dao.student import Student  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    os.environ["DATABASE_URL"],
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
